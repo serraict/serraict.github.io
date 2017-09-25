99339702f186a863b54cd0adaed95556bc38b9d0
Marijn van der Zee <marijn@serraict.com>
2017-09-25 21:16:56 +0200

    Remove unused files

--- script/htmlproof_live.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-url="https://www.serraict.com"
-
-curl "$url/sitemap.txt"  | while read page ; do bundle exec htmlproofer "$page.html" --disable-external --assume-extension --file-ignore /assets/loopy/ ; done
