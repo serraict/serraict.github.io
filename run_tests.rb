487246fb36de292d55ab893ec6428583bf337119
marijn <marijn@serraict.com>
2023-12-06 12:39:21 +0100

    Fix ci build

--- run_tests.rb
+++ run_tests.rb
@@ -3,7 +3,7 @@ require 'html-proofer'
 options = { :empty_alt_ignore => true, 
   :disable_external => true, 
   :allow_hash_href => true,
-  :assume_extension => true,
+  :url_swap => { /\A(.+)\z/ => '\1.html' },
   :file_ignore => [
     "./_site/assets/loopy/v1.1/pages/credits/index.html",
     "./_site/assets/loopy/v1.1/pages/howto.html",
