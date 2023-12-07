d8d6a1e049d4155c8c943c051eac880edb565025
marijn <marijn@serraict.com>
2023-12-07 08:29:05 +0100

    Fix html - 2 issues remaining
    Note that we accept non-https external links for now.
    There are ~150 of those links and not all can be replaced by a https counterpart.

--- run_tests.rb
+++ run_tests.rb
@@ -4,7 +4,8 @@ options = { :empty_alt_ignore => true,
   :disable_external => true, 
   :allow_hash_href => true,
   :url_swap => { /\A(.+)\z/ => '\1.html' },
-  :file_ignore => [
+  :ignore_urls => [/http:\/\//],
+  :ignore_files => [
     "./_site/assets/loopy/v1.1/pages/credits/index.html",
     "./_site/assets/loopy/v1.1/pages/howto.html",
     "./_site/assets/loopy/v1.1/pages/gif.html",
